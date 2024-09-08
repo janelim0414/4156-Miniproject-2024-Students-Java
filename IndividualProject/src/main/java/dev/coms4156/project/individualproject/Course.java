89bb2484f1978b4331b0b03dd92f731718ac5639
janelim0414 <janelim801@gmail.com>
2024-09-07 23:43:59 -0400

    Resolved checkstyle warnings

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
@@ -1,7 +1,12 @@
 package dev.coms4156.project.individualproject;
 
-import java.io.*;
+import java.io.Serializable;
 
+/**
+ * Represents a course within a department at an educational institution.
+ * This class stores information about the course, including its instructor,
+ * location, time, maximum enrollment, and number of students enrolled.
+ */
 public class Course implements Serializable {
 
   /**
@@ -20,17 +25,17 @@ public class Course implements Serializable {
     this.enrolledStudentCount = 500;
   }
 
- /**
+  /**
    * Enrolls a student in the course if there is space available.
    *
    * @return true if the student is successfully enrolled, false otherwise.
    */
   public boolean enrollStudent() {
-   enrolledStudentCount++;
+    enrolledStudentCount++;
     return false;
   }
 
- /**
+  /**
    * Drops a student from the course if a student is enrolled.
    *
    * @return true if the student is successfully dropped, false otherwise.
@@ -56,8 +61,15 @@ public class Course implements Serializable {
   }
 
 
+  /**
+   * Creates a string that represents the course.
+   *
+   * @return A string that represents the course.
+   */
   public String toString() {
-    return "\nInstructor: " + instructorName +  "; Location: "  + courseLocation +  "; Time: " + courseTimeSlot;
+    return "\nInstructor: " + instructorName
+            + "; Location: "  + courseLocation
+            + "; Time: " + courseTimeSlot;
   }
 
 
