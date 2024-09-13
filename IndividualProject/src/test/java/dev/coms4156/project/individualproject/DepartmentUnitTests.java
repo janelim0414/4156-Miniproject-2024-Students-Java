1ce0a219e91b0b6f0bf5b0d7beb5507bca1b9437
janelim0414 <janelim801@gmail.com>
2024-09-12 21:46:09 -0400

    optimized testing code using BeforeEach

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/DepartmentUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/DepartmentUnitTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.HashMap;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -17,67 +18,51 @@ import org.springframework.test.context.ContextConfiguration;
 public class DepartmentUnitTests {
 
   @BeforeAll
-  public static void setupDepartmentForTesting() {
+  public static void setupCoursesForTesting() {
     testCourse1 = new Course("Griffin Newbold", "417 IAB", "11:40-12:55", 250);
     testCourse2 = new Course("Jane Lim", "451 CSB", "17:40-18:55", 40);
   }
 
-  @Test
-  public void getNumberOfMajorsTest() {
+  @BeforeEach
+  public void setupDepartmentForTesting() {
     testCourses = new HashMap<>();
     testCourses.put("1234", testCourse1);
     testCourses.put("5678", testCourse2);
     testDepartment = new Department("COMS", testCourses, "Jae Lee", 3);
+  }
+
+  @Test
+  public void getNumberOfMajorsTest() {
     int expectedResult = 3;
     assertEquals(expectedResult, testDepartment.getNumberOfMajors());
   }
 
   @Test
   public void getDepartmentChairTest() {
-    testCourses = new HashMap<>();
-    testCourses.put("1234", testCourse1);
-    testCourses.put("5678", testCourse2);
-    testDepartment = new Department("COMS", testCourses, "Jae Lee", 3);
     String expectedResult = "Jae Lee";
     assertEquals(expectedResult, testDepartment.getDepartmentChair());
   }
 
   @Test
   public void getCourseSelectionTest() {
-    testCourses = new HashMap<>();
-    testCourses.put("1234", testCourse1);
-    testCourses.put("5678", testCourse2);
-    testDepartment = new Department("COMS", testCourses, "Jae Lee", 3);
     HashMap<String, Course> expectedResult = testCourses;
     assertEquals(expectedResult, testDepartment.getCourseSelection());
   }
 
   @Test
   public void addPersonToMajorTest() {
-    testCourses = new HashMap<>();
-    testCourses.put("1234", testCourse1);
-    testCourses.put("5678", testCourse2);
-    testDepartment = new Department("COMS", testCourses, "Jae Lee", 3);
     testDepartment.addPersonToMajor();
     assertEquals(4, testDepartment.getNumberOfMajors());
   }
 
   @Test
   public void dropPersonFromMajorTest() {
-    testCourses = new HashMap<>();
-    testCourses.put("1234", testCourse1);
-    testCourses.put("5678", testCourse2);
-    testDepartment = new Department("COMS", testCourses, "Jae Lee", 3);
     testDepartment.dropPersonFromMajor();
     assertEquals(2, testDepartment.getNumberOfMajors());
   }
 
   @Test
   public void addCourseTest() {
-    testCourses = new HashMap<>();
-    testCourses.put("1234", testCourse1);
-    testCourses.put("5678", testCourse2);
-    testDepartment = new Department("COMS", testCourses, "Jae Lee", 3);
     Course newCourse = new Course("Gail Kaiser", "417 IAB", "10:10-11:25", 120);
     HashMap<String, Course> expectedResult = testCourses;
     expectedResult.put("2345", newCourse);
@@ -87,10 +72,6 @@ public class DepartmentUnitTests {
 
   @Test
   public void createCourseTest() {
-    testCourses = new HashMap<>();
-    testCourses.put("1234", testCourse1);
-    testCourses.put("5678", testCourse2);
-    testDepartment = new Department("COMS", testCourses, "Jae Lee", 3);
     String expectedResult =
             "COMS 1234: \nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55\n"
                     + "COMS 2345: \nInstructor: Gail Kaiser; Location: 417 IAB; Time: 10:10-11:25\n"
@@ -101,20 +82,12 @@ public class DepartmentUnitTests {
 
   @Test
   public void createCourseEmptyParameterTest() {
-    testCourses = new HashMap<>();
-    testCourses.put("1234", testCourse1);
-    testCourses.put("5678", testCourse2);
-    testDepartment = new Department("COMS", testCourses, "Jae Lee", 3);
     assertThrows(IllegalArgumentException.class,
             () -> testDepartment.createCourse("", "Gail Kaiser", "417 IAB", "10:10-11:25", 120));
   }
 
   @Test
   public void toStringTest() {
-    testCourses = new HashMap<>();
-    testCourses.put("1234", testCourse1);
-    testCourses.put("5678", testCourse2);
-    testDepartment = new Department("COMS", testCourses, "Jae Lee", 3);
     String expectedResult =
             "COMS 1234: \nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55\n"
             + "COMS 5678: \nInstructor: Jane Lim; Location: 451 CSB; Time: 17:40-18:55\n";
